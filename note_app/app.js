28d8b3ed1a91a839c27579617ff6419e152d4e88
George Richards <g.richards101@gmail.com>
2018-03-22 12:32:01 +0000

    capitalised first letter of OS username

--- note_app/app.js
+++ note_app/app.js
@@ -5,7 +5,7 @@ const os = require('os');
 
 let user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, (err) => {
+fs.appendFile('greetings.txt', `Hello ${user.username.charAt(0).toUpperCase() + user.username.slice(1)}!`, (err) => {
   if (err) {
     console.log('Unable to write to file!');
   }
