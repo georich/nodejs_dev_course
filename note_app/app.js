e9bfae3368eb6228decd3f408eff2f99e22f952e
George Richards <g.richards101@gmail.com>
2018-03-22 15:08:41 +0000

    using yargs to accept inputs via the cli

--- note_app/app.js
+++ note_app/app.js
@@ -3,8 +3,31 @@ console.log('Starting app.js');
 const fs = require('fs');
 // const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
+let command = process.argv[2];
+console.log(`Command: ${command}`);
+// console.log('Process', process.argv);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  // console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  // console.log('Listing all notes')
+  notes.getAll();
+} else if (command === 'read') {
+  // console.log('Reading note');
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  // console.log('Removing note');
+  notes.removeNote(argv.title);
+} else {
+  console.log('Commannd not recognised');
+}
 
 // console.log(_.isString(true));
 // console.log(_.isString('George'));
--- note_app/notes.js
+++ note_app/notes.js
@@ -1,9 +1,32 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+let addNote = (title, body) => {
+  console.log('Adding note', title, body);
 };
+
+let getAll = () => {
+  console.log('Getting all notes');
+};
+
+let getNote = (title) => {
+  console.log('Reading note', title)
+};
+
+let removeNote = (title) => {
+  console.log('Removing note', title)
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+};
+
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   return 'New note';
+// };
 
 // module.exports.add = (a, b) => {
 //   return a + b;
