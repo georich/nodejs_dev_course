a337c3c47c828491c788a5e32f77c148aed2e967
George Richards <g.richards101@gmail.com>
2018-03-22 13:46:00 +0000

    importing a custom function

--- note_app/app.js
+++ note_app/app.js
@@ -4,10 +4,15 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js')
 
-let user = os.userInfo();
+let res = notes.addNote();
+console.log(res);
 
-fs.appendFile('greetings.txt', `Hello ${user.username.charAt(0).toUpperCase() + user.username.slice(1)}! You are ${notes.age}.`, (err) => {
-  if (err) {
-    console.log('Unable to write to file!');
-  }
-});
+console.log(notes.add(1, 3));
+
+// let user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `Hello ${user.username.charAt(0).toUpperCase() + user.username.slice(1)}! You are ${notes.age}.`, (err) => {
+//   if (err) {
+//     console.log('Unable to write to file!');
+//   }
+// });
--- note_app/notes.js
+++ note_app/notes.js
@@ -1,3 +1,10 @@
 console.log('Starting notes.js');
 
-module.exports.age = 24;
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
