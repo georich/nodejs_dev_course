2d29ae02661647f6d842ac1311a532665767b754
George Richards <g.richards101@gmail.com>
2018-03-22 12:37:37 +0000

    simple example of modules exports

--- note_app/app.js
+++ note_app/app.js
@@ -1,11 +1,12 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
 let user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username.charAt(0).toUpperCase() + user.username.slice(1)}!`, (err) => {
+fs.appendFile('greetings.txt', `Hello ${user.username.charAt(0).toUpperCase() + user.username.slice(1)}! You are ${notes.age}.`, (err) => {
   if (err) {
     console.log('Unable to write to file!');
   }
