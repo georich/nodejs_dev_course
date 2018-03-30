a5c23d0f937036205187c012e348723a861892ac
George Richards <g.richards101@gmail.com>
2018-03-30 12:43:08 +0100

    api key added

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia&key=AIzaSyA9IOBTC9RCjuTR3QChNOVEu9Ri0yX3k4U',
   json: true
 }, (error, response, body) => {
   console.log(body);
