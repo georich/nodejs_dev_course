1488aa51762c102ffe5d03110ac9b771a82bf8f6
George Richards <g.richards101@gmail.com>
2018-03-30 12:47:02 +0100

    changed api returned object to be pretty printed

--- weather_app/app.js
+++ weather_app/app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia&key=AIzaSyA9IOBTC9RCjuTR3QChNOVEu9Ri0yX3k4U',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
