8686ef96dd130482ab8f4341ef7b1284011dad95
George Richards <g.richards101@gmail.com>
2018-03-30 19:49:33 +0100

    clean up geocode

--- weather_app/geocode/geocode.js
+++ weather_app/geocode/geocode.js
@@ -9,19 +9,14 @@ let geocodeAddress = (address, callback) => {
   }, (error, response, body) => {
     if (error) {
       callback('Unable to connect to Google servers.');
-      // console.log('Unable to connect to Google servers.');
     } else if (body.status === 'ZERO_RESULTS') {
       callback('Unable to find that address.');
-      // console.log('Unable to find that address.');
     } else if (body.status === 'OK') {
       callback(undefined, {
         address: body.results[0].formatted_address,
         latitude: body.results[0].geometry.location.lat,
         longitude: body.results[0].geometry.location.lng
       });
-      // console.log(`Address: ${body.results[0].formatted_address}`);
-      // console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      // console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
     }
   });
 };
