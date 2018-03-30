c7fa52699ec1f8d0f8c726afb8ab1dd55e1df081
George Richards <g.richards101@gmail.com>
2018-03-30 18:55:12 +0100

    add ability to enter custom address via yargs and encodeURI

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,7 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv
+
+// console.log(argv);
+encodedAddress = encodeURIComponent(argv.address);
+// console.log(encodedAddress);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia&key=AIzaSyA9IOBTC9RCjuTR3QChNOVEu9Ri0yX3k4U',
+  // url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+street+philadelphia&key=AIzaSyA9IOBTC9RCjuTR3QChNOVEu9Ri0yX3k4U',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA9IOBTC9RCjuTR3QChNOVEu9Ri0yX3k4U`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
