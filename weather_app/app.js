f6e3c95a79a622affc7c6b7b6f70ae2ea95e8660
George Richards <g.richards101@gmail.com>
2018-03-31 17:29:59 +0100

    clean up old comments

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,5 +1,4 @@
 const yargs = require('yargs');
-// const request = require('request');
 
 const geocode = require('./geocode/geocode.js');
 const weather = require('./weather/weather.js');
@@ -21,28 +20,13 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    // console.log(JSON.stringify(results, undefined, 2));
     console.log(results.address);
     weather.getWeather(results.latitude, results.longitude, (errorMessage, results) => {
       if (errorMessage) {
         console.log(errorMessage);
       } else {
-        // console.log(JSON.stringify(results, undefined, 2));
         console.log(`The current temparure is ${results.temperature} Fahrenheit, it feels like ${results.apparentTemperature} Fahrenheit.`);
       }
     });
   }
 });
-
-// 7aab4afb7ad387ca11385ced7b3c041e API key for dark sky
-/*
-const latitude = 52.9486597;
-const longitude = -1.1737802;
-weather.getWeather(latitude, longitude, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
-  } else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
-*/
