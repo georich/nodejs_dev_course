7b54f7e6851d0d97b85e7f404be32c5786c9de49
George Richards <g.richards101@gmail.com>
2018-04-02 15:56:09 +0100

    basic templating added using hbs

--- web_server/server.js
+++ web_server/server.js
@@ -1,22 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'George',
-    likes: [
-      'Cats',
-      'Alice'
-    ]
+  // res.send({
+  //   name: 'George',
+  //   likes: [
+  //     'Cats',
+  //     'Alice'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my home page! I am learning templating for express',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About Page</h1>');
+  // res.send('<h1>About Page</h1>');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
