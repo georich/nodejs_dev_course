872b34017e80c8a00c9839058f425cca52d930cb
George Richards <g.richards101@gmail.com>
2018-04-04 14:36:19 +0100

    using describe() to categorise tests

--- testing_mocha/utils/utils.test.js
+++ testing_mocha/utils/utils.test.js
@@ -2,64 +2,71 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  let res = utils.add(33, 11);
+describe('Utils', () => {
 
-  expect(res).toBe(44);
-  expect(typeof res).toBe('number');
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}`);
-  // }
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7);
-    expect(typeof sum).toBe('number');
-    done();
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      let res = utils.add(33, 11);
+    
+      expect(res).toBe(44);
+      expect(typeof res).toBe('number');
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44, but got ${res}`);
+      // }
+    });
   });
-});
-
-it('should square a number', () => {
-  let res = utils.square(9);
-
-  expect(res).toBe(81);
-  expect(typeof res).toBe('number');
-  // if (res !== 81) {
-  //   throw new Error(`Expected 81, but got ${res}`);
-  // }
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(9, (res) => {
+    
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7);
+      expect(typeof sum).toBe('number');
+      done();
+    });
+  });
+  
+  it('should square a number', () => {
+    let res = utils.square(9);
+  
     expect(res).toBe(81);
     expect(typeof res).toBe('number');
-    done();
+    // if (res !== 81) {
+    //   throw new Error(`Expected 81, but got ${res}`);
+    // }
+  });
+  
+  it('should async square a number', (done) => {
+    utils.asyncSquare(9, (res) => {
+      expect(res).toBe(81);
+      expect(typeof res).toBe('number');
+      done();
+    });
   });
 });
 
-it('should expect some values', () => {
-  // expect(12).not.toBe(11);
-  // expect({name: 'george'}).not.toEqual({name: 'George'});
-  // expect([2, 3, 4]).not.toContain(5);
-  expect({
-    name: 'George',
-    age: 24,
-    location: 'Nottingham'
-  }).not.toMatchObject({
-    age: 23
+describe('Examples', () => {
+  it('should expect some values', () => {
+    // expect(12).not.toBe(11);
+    // expect({name: 'george'}).not.toEqual({name: 'George'});
+    // expect([2, 3, 4]).not.toContain(5);
+    expect({
+      name: 'George',
+      age: 24,
+      location: 'Nottingham'
+    }).not.toMatchObject({
+      age: 23
+    });
   });
-});
-
-it('should verify first and last names are set', () => {
-  let res = utils.setName({
-    age: 24,
-    location: 'Nottingham'
-  }, 'George Richards');
-
-  expect(res).toMatchObject({
-    firstName: 'George',
-    lastName: 'Richards'
+  
+  it('should verify first and last names are set', () => {
+    let res = utils.setName({
+      age: 24,
+      location: 'Nottingham'
+    }, 'George Richards');
+  
+    expect(res).toMatchObject({
+      firstName: 'George',
+      lastName: 'Richards'
+    });
+    expect(typeof res).toBe('object');
   });
-  expect(typeof res).toBe('object');
 });
