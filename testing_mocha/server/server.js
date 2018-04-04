b61d12e8c1850e8f5a15be7dd33d94ed0e2adbb5
George Richards <g.richards101@gmail.com>
2018-04-04 13:20:33 +0100

    testing express code with supertest and expect

--- testing_mocha/server/server.js
+++ testing_mocha/server/server.js
@@ -4,9 +4,22 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
 });
+
+app.get('/users', (req, res) => {
+  res.send([
+    {name: 'George', age: 24},
+    {name: 'Alice', age: 21},
+    {name: 'Bonje', age: 22}
+  ]);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+module.exports.app = app;
---
+++ testing_mocha/server/server.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const expect = require('expect');
+
+const app = require('./server').app;
+
+it('should return 404 response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    // .expect({
+    //   error: 'Page not found.'
+    // })
+    .expect((res) => {
+      expect(res.body).toMatchObject({
+        error: 'Page not found.'
+      });
+    })
+    .end(done);
+});
+
+it('should return my user object', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toContainEqual({name: 'George', age: 24});
+    })
+    .end(done);
+});
