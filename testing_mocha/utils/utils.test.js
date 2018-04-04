388c732aaa5ececdb4a86c3abcc2d0cc83e8c2ad
George Richards <g.richards101@gmail.com>
2018-04-04 12:21:47 +0100

    more advanced testing using the expect package

--- testing_mocha/utils/utils.test.js
+++ testing_mocha/utils/utils.test.js
@@ -1,17 +1,49 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44);
+  expect(typeof res).toBe('number');
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}`);
+  // }
 });
 
 it('should square a number', () => {
   let res = utils.square(9);
 
-  if (res !== 81) {
-    throw new Error(`Expected 81, but got ${res}`);
-  }
+  expect(res).toBe(81);
+  expect(typeof res).toBe('number');
+  // if (res !== 81) {
+  //   throw new Error(`Expected 81, but got ${res}`);
+  // }
+});
+
+it('should expect some values', () => {
+  // expect(12).not.toBe(11);
+  // expect({name: 'george'}).not.toEqual({name: 'George'});
+  // expect([2, 3, 4]).not.toContain(5);
+  expect({
+    name: 'George',
+    age: 24,
+    location: 'Nottingham'
+  }).not.toMatchObject({
+    age: 23
+  });
+});
+
+it('should verify first and last names are set', () => {
+  let res = utils.setName({
+    age: 24,
+    location: 'Nottingham'
+  }, 'George Richards');
+
+  expect(res).toMatchObject({
+    firstName: 'George',
+    lastName: 'Richards'
+  });
+  expect(typeof res).toBe('object');
 });
