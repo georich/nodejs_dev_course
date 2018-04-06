10d165faaebd3fe645316f67b73b6e5512f89caf
George Richards <g.richards101@gmail.com>
2018-04-06 13:22:34 +0100

    test POST /todos route

--- todo_api/server/server.js
+++ todo_api/server/server.js
@@ -26,34 +26,6 @@ app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
 
-// let newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo')
-// });
-
-// let newTodo = new Todo({
-//   text: 'Revise physics',
-//   completed: true,
-//   completedAt: 190318
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log(`Saved todo ${doc}`);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
-// let newUser = new User({
-//   email: 'g.richards101@gmail.com'
-// });
-
-// newUser.save().then((doc) => {
-//   console.log(`Saved user ${JSON.stringify(doc, undefined, 2)}`);
-// }, (e) => {
-//   console.log('Unable to save', e);
-// });
+module.exports = {
+  app
+};
