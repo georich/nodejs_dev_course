759d66693212f4d20481468b3e4d2ec8b29c6bc2
George Richards <g.richards101@gmail.com>
2018-04-11 11:40:40 +0100

    add tests for POST /users/login

--- todo_api/server/tests/server.test.js
+++ todo_api/server/tests/server.test.js
@@ -241,3 +241,50 @@ describe('GET /users/me', () => {
       .end(done);
   });
 });
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: users[1].password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.email).toBe(users[1].email);
+        expect(res.body._id).toBe(users[1]._id.toHexString());
+        expect(res.header['x-auth']).toBeTruthy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toMatchObject({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email: users[1].email, password: 'incorrectPassword'})
+      .expect(400)
+      .expect((res) => {
+        expect(res.header['x-auth']).toBeFalsy();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
