d14ca74689687e11626debb488614651ef7c68a0
George Richards <g.richards101@gmail.com>
2018-04-12 16:16:55 +0100

    add newMessage and createMessage events

---
+++ chat_app/public/js/index.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    to: 'Alice',
+    text: 'Hey there!'
+  });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', (message) => {
+  console.log('New message', message);
+});
--- chat_app/server/server.js
+++ chat_app/server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'George',
+    text: 'Hello!',
+    createdAt: 28395
+  });
+
+  socket.on('createMessage', (data) => {
+    console.log('createMessage', data);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
