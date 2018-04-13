d93f7172b8ef131df20ffb38471b294021510b8d
George Richards <g.richards101@gmail.com>
2018-04-13 12:40:10 +0100

    add form and event listeners for messages

--- chat_app/public/js/index.js
+++ chat_app/public/js/index.js
@@ -15,11 +15,27 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('New message', message);
+
+  let li = document.createElement('li');
+  li.textContent = `${message.from}: ${message.text}`;
+
+  document.getElementById('messages').appendChild(li);
 });
 
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, (data) => {
-  console.log('Got it', data);
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, (data) => {
+//   console.log('Got it', data);
+// });
+
+document.getElementById('message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: document.getElementsByName('message')[0].value
+  }, () => {
+
+  });
 });
